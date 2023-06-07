d90af49e4bb59698a5b9ae9d2e88f03719ac8b40
gsk1307 <132924253+gsk1307@users.noreply.github.com>
2023-06-07 19:24:33 +0530

    Add files via upload
    5 5 5 5 5 
    4 4 4 4
    3 3 3
    2 2
    1
    1 2 3 4 5
    1 2 3 4
    1 2 3
    1 2
    1

---
+++ num_pat4.py
@@ -0,0 +1,12 @@
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(i,end=' ')
+    print()
+
+#replacing i
+
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print()
+
